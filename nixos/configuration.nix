fa3ebf4ecdddf976d9d9fbf942f361e5fd8cb8d8
lucastso10 <lucastso10@gmail.com>
2023-10-06 10:19:58 -0300

    Customizing firefox

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -187,12 +187,8 @@
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      git
+     myFirefox
   ];
- 
-  programs.firefox = {
-    enable = true;
-    package = myFirefox;
-  };
    
 
   virtualisation.docker.enable = true;
