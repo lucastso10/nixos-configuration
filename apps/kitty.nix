69e780f69103702bba7e8dd2e23d2ef38a797cf7
lucastso10 <lucastso10@gmail.com>
2023-11-07 19:00:30 -0300

    new kitty theme and starship

--- apps/kitty.nix
+++ apps/kitty.nix
@@ -18,7 +18,26 @@
     home-manager.users."bolofofo" = { pkgs, ... }:{
       programs.kitty = {
         enable = true;
-        theme = "Batman";
+        theme = "Alucard";
+      };
+
+      programs.starship = {
+        enable = true;
+        enableBashIntegration = true;
+        settings = {
+          add_newline = false;
+          format = lib.concatStrings [
+            "$line_break"
+            "$package"
+            "$line_break"
+            "$character"
+          ];
+          scan_timeout = 10;
+          character = {
+            success_symbol = "➜";
+            error_symbol = "➜";
+          }; 
+        };
       };
     };
     
