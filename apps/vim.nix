cada0ba5d749fdaa6c8bd072a4102d98daf36c88
lucastso10 <lucastso10@gmail.com>
2023-11-10 09:58:59 -0300

    messing with vim

--- apps/vim.nix
+++ apps/vim.nix
@@ -20,6 +20,8 @@
       enable = true;
       vimAlias = true;
 
+      clipboard.providers.wl-copy.enable = true; # only true for wayland systems, may want to alter this later
+
       options = {
         relativenumber = true;
         shiftwidth = 2;
@@ -29,19 +31,44 @@
       colorschemes.tokyonight.enable = true;
 
       plugins = {
+        # highlighting stuff
         nix.enable = true;
-        lightline.enable = true;
         treesitter.enable = true;
         treesitter-context.enable = true;
-        trouble.enable = true;
 
+        # trying to do autocomplete NOT WORKING
+        lsp = {
+          enable = true;
+          servers = {
+            clangd.enable = true; # C/C++
+            cmake.enable = true; # cmake
+            digestif.enable = true; # LaTex
+            nixd.enable = true; # nix
+            pylsp.enable = true; # python
+          };
+        };
         cmp-treesitter.enable = true;
-        lsp.enable = true;
+        cmp-nvim-lsp.enable = true;
+        cmp-buffer.enable = true;
         nvim-cmp = {
           enable = true;
-          autoEnableSources = true;
         };
+
+        # browse through files
         neo-tree.enable = true;
+
+        # button bar
+        airline = {
+          enable = true;
+          powerline = true;
+          theme = null;
+        };
+
+        # tabs
+        bufferline = {
+          enable = true;
+          diagnostics = "nvim_lsp";
+        };
       };
 
       extraPlugins = with pkgs.vimPlugins; [
