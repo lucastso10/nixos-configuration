b803a69dd586ffbc519a6409e3db7987dfcfe969
lucastso10 <lucastso10@gmail.com>
2023-12-06 03:40:49 -0300

    changing firefox to home-manager

--- apps/firefox.nix
+++ apps/firefox.nix
@@ -1,35 +1,6 @@
 {config, lib, pkgs, ...}:
 
-let
-  myFirefox = with pkgs; wrapFirefox firefox-esr-unwrapped {
-    nixExtensions = [
-      (fetchFirefoxAddon {
-        name = "ublock"; # Has to be unique!
-        url = "https://addons.mozilla.org/firefox/downloads/file/4171020/ublock_origin-1.52.2.xpi";
-        hash = "sha256-6O4/nVl6bULbnXP+h8HVId40B1X9i/3WnkFiPt/gltY=";
-      }) 
-
-      (fetchFirefoxAddon {
-        name = "Purple-Night-Theme";
-        url = "https://addons.mozilla.org/firefox/downloads/file/3620954/purple_night_theme-1.0.xpi";
-        hash = "sha256-7PE3aXM7gOqleS0iYzrGRb/z7XtQpThrnH3eusTYXEk=";
-      })
-
-      (fetchFirefoxAddon {
-        name = "bttv";
-        url = "https://addons.mozilla.org/firefox/downloads/file/3620954/purple_night_theme-1.0.xpi";
-        hash = "sha256-7PE3aXM7gOqleS0iYzrGRb/z7XtQpThrnH3eusTYXEk=";
-      })
-    ];
-
-     extraPolicies = {
-      DisableFirefoxStudies = true;
-      DisablePocket = true;
-    };
-  };
-
-in {
-
+{
   options = with lib; {
     apps.firefox = {
       enable = mkOption {
@@ -43,8 +14,21 @@ in {
   };
 
   config = lib.mkIf config.apps.firefox.enable {
-    environment.systemPackages = with pkgs; [
-      myFirefox
-    ];
+    home-manager.users."bolofofo" = {pkgs, ...}:{
+      programs.firefox = {
+        enable = true;
+        # gnomeExtesion = config.gnome.enable;
+
+        policies = {
+          DefaultDownloadDirectory = "\${home}/Downloads";
+          ExtensionSettings.installation_mode = "allowed";
+          FirefoxHome = {
+            SponsoredTopSites = false;
+            SponsoredPocket = false;
+          };
+        };
+
+      };
+    };
   };
 }
