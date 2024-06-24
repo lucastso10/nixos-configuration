81feabd400eb9970c3e276bd014fcdfe34245e61
lucastso10 <lucastso10@gmail.com>
2024-06-24 14:37:15 -0300

    added comment

--- desktop/shell/direnv.nix
+++ desktop/shell/direnv.nix
@@ -3,6 +3,10 @@
   # direnv is used to automatically enable enviroments when moving through your system
   # to automatically enter a enviroment there has to be .envrc with the commands needed
   # if you are using a shell.nix or have a default.nix create a .envrc with just "use nix"
+  # 
+  # for this to work you have to make sure direnv is hooked to the shell
+  # run this command to hook it:
+  # eval "$(direnv hook bash)"
   config = lib.mkIf config.desktop.shell.enable {
 
     home-manager.users."bolofofo" = { pkgs, ... }: {
