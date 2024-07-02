3c4dc0d25a9fde0e876b8afe851581865716fa07
lucastso10 <lucastso10@gmail.com>
2024-07-02 15:44:24 -0300

    changed hardware setting for solid after formatting disk

--- systems/solid/hardware.nix
+++ systems/solid/hardware.nix
@@ -4,20 +4,27 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/a1527915-3dc2-4d8e-8ced-c898279d82e8";
-    fsType = "ext4";
-  };
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/1720-36B6";
-    fsType = "vfat";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/e701cd1e-dc39-48dd-ab50-3fb50e40647a";
+      fsType = "ext4";
+    };
 
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/5178f0e4-9f4f-4457-8009-2761c7f5ee30"; }];
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/64A2-AB86";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -28,7 +35,5 @@
   # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
