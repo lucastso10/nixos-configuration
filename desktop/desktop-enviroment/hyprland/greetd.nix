d83c5d47f05e34235af9adad1b7e67a67109b383
lucastso10 <lucastso10@gmail.com>
2024-07-29 15:06:01 -0300

    Revert "trying to make rofi good"
    This reverts commit 9ced403d853b84403a8a10359bdde2ccd56da40a.

--- desktop/desktop-enviroment/hyprland/greetd.nix
+++ desktop/desktop-enviroment/hyprland/greetd.nix
@@ -10,12 +10,12 @@
   config = lib.mkIf config.desktop.hyprland.enable {
     services.greetd = {
       enable = true;
-      settings = rec {
-        initial_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --cmd Hyprland";
+      settings = {
+        default_session = {
+          command =
+            "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --cmd Hyprland";
           user = "bolofofo";
         };
-        default_session = initial_session;
       };
     };
   };
