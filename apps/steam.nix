fd72afc27eda0d5b28f0a02e009e10acff8cdc72
lucastso10 <lucastso10@gmail.com>
2026-01-21 23:19:48 -0300

    few steam config changes

--- apps/steam.nix
+++ apps/steam.nix
@@ -13,6 +13,10 @@
   };
 
   config = lib.mkIf config.apps.steam.enable {
-    programs.steam.enable = true;
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    };
+    hardware.steam-hardware.enable = true;
   };
 }
