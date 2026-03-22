46d880ec0e241fab9e7f7cb9936eaac6dab9084a
lucastso10 <lucastso10@gmail.com>
2026-03-22 13:43:12 -0300

    adjusting solid config

--- systems/solid/default.nix
+++ systems/solid/default.nix
@@ -11,26 +11,26 @@
     # Configure console keymap
     console.keyMap = "br-abnt2";
 
-    desktop.shell.enable = true;
-    desktop.nerdfont.enable = true;
-    desktop.hyprland.enable = true;
-    desktop.hyprland.monitors = [
-      "HDMI-A-1, 1920x1080@180, 0x0, 1"
-      "DP-3, 1920x1080@60, -1920x0, 1"
-    ];
+    desktop = {
+      shell.enable = true;
+      nerdfont.enable = true;
+      hyprland.enable = true;
+    };
 
-    apps.ssh.enable = false;
-    apps.virt-manager.enable = true;
-    apps.vim.enable = true;
-    apps.docker.enable = true;
-    apps.tmux.enable = true;
-
-    apps.firefox.enable = true;
-    apps.kitty.enable = true;
-    apps.prism.enable = true;
-    apps.popcorntime.enable = true;
-    apps.ghostty.enable = true;
-    apps.steam.enable = true;
+    apps = {
+      #ssh.enable = false;
+      #virt-manager.enable = true;
+      vim.enable = true;
+      docker.enable = true;
+      tmux.enable = true;
+      #firefox.enable = true;
+      prism.enable = true;
+      popcorntime.enable = true;
+      steam.enable = true;
+      discord.enable = true;
+      ghostty.enable = true;
+      zen-browser.enable = true;
+    };
 
     networking.firewall.enable = true;
   };
